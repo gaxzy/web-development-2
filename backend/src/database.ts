3cdd4ce49b1ee655fd30c0b21806dca76d628e0a
gaxzy <daan@gaaj.dev>
2025-02-15 17:37:38 +0100

    Remove sync from server.ts

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -4,20 +4,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME || "notes_database",
-    process.env.DB_USER || "user",
-    process.env.DB_PASSWORD || "userp",
-    {
-        dialect: "mariadb",
-        host: process.env.DB_HOST || "notes_mariadb",
-        port: Number(process.env.DB_PORT) || 3306,
-        logging: console.log,
-    }
+  process.env.DB_NAME || "notes_database",
+  process.env.DB_USER || "user",
+  process.env.DB_PASSWORD || "userp",
+  {
+    dialect: "mariadb",
+    host: process.env.DB_HOST || "notes_mariadb",
+    port: Number(process.env.DB_PORT) || 3306,
+    logging: console.log,
+  },
 );
 
+// Database Sync
 sequelize
-    .authenticate()
-    .then(() => console.log("Database connected!"))
-    .catch((err) => console.error("Error connection to database:", err));
+  .sync({ alter: true })
+  .then(() => console.log("Database synchronized!"))
+  .catch((err) => console.error("Error syncing database:", err));
 
 export default sequelize;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import routes from "./routes";
-import sequelize from "./database";
 
 dotenv.config();
 
@@ -12,20 +11,14 @@ const PORT = Number(process.env.BACKEND_PORT || 5000);
 app.use(cors());
 app.use(express.json());
 
-// Database Sync
-sequelize
-    .sync({ alter: true }) // Ensures DB schema updates
-    .then(() => console.log("Database synchronized!"))
-    .catch((err) => console.error("Error syncing database:", err));
-
 // Routes
 app.use("/api", routes);
 
 // Health check endpoint
 app.get("/", (req, res) => {
-    res.send("Backend is running");
+  res.send("Backend is running");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
