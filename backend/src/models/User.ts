86f5cdf6cc92734ff5aad597e92a7bc1e2585c35
gaxzy <daan@gaaj.dev>
2025-02-15 17:35:43 +0100

    Update passord hash on update

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -3,53 +3,58 @@ import sequelize from "../database";
 import bcrypt from "bcryptjs";
 
 class User extends Model {
-    public id!: number;
-    public username!: string;
-    public email!: string;
-    public password!: string;
-    public created_at!: Date;
+  public id!: number;
+  public username!: string;
+  public email!: string;
+  public password!: string;
+  public created_at!: Date;
 
-    async checkPassword(password: string) {
-        return bcrypt.compare(password, this.password);
-    }
+  async checkPassword(password: string) {
+    return bcrypt.compare(password, this.password);
+  }
 }
 
 User.init(
-    {
-        id: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        username: {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-            unique: true,
-        },
-        email: {
-            type: DataTypes.STRING(255),
-            allowNull: false,
-            unique: true,
-        },
-        password: {
-            type: DataTypes.CHAR(60),
-            allowNull: false,
-        },
-        created_at: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        sequelize,
-        tableName: "Users",
-        timestamps: false,
-        hooks: {
-            beforeCreate: async (user) => {
-                user.password = await bcrypt.hash(user.password, 10);
-            },
-        },
-    }
+    username: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.CHAR(60),
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    tableName: "Users",
+    timestamps: false,
+    hooks: {
+      beforeCreate: async (user) => {
+        user.password = await bcrypt.hash(user.password, 10);
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed("password")) {
+          user.password = await bcrypt.hash(user.password, 10);
+        }
+      },
+    },
+  },
 );
 
 export default User;
